c04103e73b297ab2c52c309d9215df2ee8414afb
Mixasikus <mixasiikus@gmail.com>
2022-08-14 22:31:44 +0300

    .

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,27 +1,12 @@
-import { TableTransactions, TransactionsContainer, TableHead } from "./TransactionHistory.styled"
+import TransactionTable from "./TransactionTable"
+import {TableTransactions, TableHead} from "./TransactionHistory.styled"
 
 export default function TransactionHistory({ items }) {
-    return (<div>{items.map(item => (<TransactionsContainer key={item.id}><TableTransactions>
-        <TableHead>
+    return (<TableTransactions><TableHead>
     <tr>
       <th>Type</th>
       <th>Amount</th>
       <th>Currency</th>
     </tr>
-    </TableHead>
-       
-  <tbody>
-    <tr>
-      <td>{item.type}</td>
-      <td>{item.amount}</td>
-      <td>{item.currency}</td>
-    </tr>
-    </tbody>
-         </TableTransactions></TransactionsContainer>))}</div> )
+  </TableHead>{items.map(item =>(<TransactionTable key={item.id} type={item.type} amount={item.amount} currency={item.currency}></TransactionTable>))}</TableTransactions> )
 }
-
-//  <tr>
-//       <td>Withdrawal</td>
-//       <td>85</td>
-//       <td>USD</td>
-//     </tr>
--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -2,9 +2,5 @@ import styled from "@emotion/styled";
 
 export const TableTransactions = styled.table`
 `
-
-export const TransactionsContainer = styled.div`
-`
-
 export const TableHead = styled.thead`
 `
